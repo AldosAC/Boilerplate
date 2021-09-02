71e1a828fdd231e87f94fbd75f3be83da879fe30
Joel Carpenter <joel.carpenter@slalom.com>
2021-09-02 13:54:19 -0700

    Initialize boilerplate

---
+++ README.md
@@ -0,0 +1,5 @@
+#Boilerplate Repo
+
+This is a boilerplate React app!
+
+Includes webpack setup and live-server to serve the front end.
---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+
+const App = (props) => {
+
+  return (
+    <div className="main-container">
+      You've successfully loaded the boilerplate React app.
+    </div>
+  )
+};
+
+export default App;
